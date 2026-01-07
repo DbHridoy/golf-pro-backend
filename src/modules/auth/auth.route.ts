edd31f47e1030ccd9f520cf47f92768108b16524
DbHridoy <mdnuralam378@gmail.com>
2026-01-07 16:49:19 +0600

    fix(validation): again put back the validation middleware for authentication

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
-import { authMiddleware } from "@/middlewares/auth.middleware";
+import { validate } from "@/middlewares/validate.middleware";
 
 import { authController } from "./auth.controller";
+import { createUserSchema } from "./auth.schema";
 
 const router = Router();
 
-router.post("/register", authController.register);
+router.post("/register", validate(createUserSchema), authController.register);
 router.post("/login", authController.login);
 router.post("/send-otp", authController.sendOtp);
 router.post("/verify-otp", authController.verifyOtp);
@@ -14,6 +15,6 @@ router.post("/set-password", authController.setNewPassword);
 // router.post("/google-login", authController.googleLogin);
 // router.post("/ghin-login", authController.ghinLogin);
 router.post("/refresh", authController.refreshToken);
-router.post("/logout",  authController.logout);
+router.post("/logout", authController.logout);
 
 export default router;
